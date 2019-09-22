16a1f42675564b2c0e6db8bf7276aeefece09127
Issame SARROUKH <Issame.SARROUKH@amadeus.com>
2019-09-22 23:17:14 +0200

    done some changes to algorithm.py and correcting some bugs

--- utils/algorithms.py
+++ utils/algorithms.py
@@ -1,14 +1,45 @@
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
-def model( X_train=None, parameters=None, name = "random forest", search='randomize search',):
-    models = {
-        "random forest": RandomForestClassifier(),
-        "naive bayes":GaussianNB(),
-        "":,
-        "":,
-        "":,
-            }
+
+def model(X_train=None, y_train=None, name="random forest", search='randomize search', type='cs'):
+    """
+    Return a trained model, paramaters are to be specified
+
+    :param type: type of the machine learning algorihm either classification or regression, "cs" for classification,
+    anything for regression
+    :param X_train: Training dataset
+    :param parameters: parameters
+    :param name: name of the model for classification or regression
+    :param search: search method to find the best parameters
+    :return: a trained model to be evaluated
+    """
+    models = dict()
+
+    if type == "cs":
+        models = {
+            "random forest": RandomForestClassifier(),
+            "naive bayes": GaussianNB(),
+        }
+    else:
+        models = {}
+
+    if type == "cs":
+        param_grid = {
+            "random forest": RandomForestClassifier(),
+            "naive bayes": GaussianNB(),
+        }
+    else:
+        param_grid = {
+            "random forest": RandomForestClassifier(),
+            "naive bayes": GaussianNB(),
+        }
+
+    clf = models[name]
+
+    search_methods = {'randomized search': {RandomizedSearchCV(clf, param_distributions=param_grid, scoring='')},
+                      'grid search': {GridSearchCV(clf, param_grid=param_grid, scoring='')}
+                      }
+
+    search_method = search_methods[search]
